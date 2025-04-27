b687d5efa167b01193daa5d239ad563ad643b5c2
lordboba <yxiao1717@gmail.com>
2025-04-27 04:24:55 -0700

    fixed CORS

--- backend2.0/main.py
+++ backend2.0/main.py
@@ -34,6 +34,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://lahacks-tau.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
